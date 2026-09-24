45c864fb540f9c0b5b54a937789f658055bc8678
deebsie <135828695+deebsie@users.noreply.github.com>
2026-09-24 15:31:26 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = `THIS FOR U LALANYA MAS`.split('')
+const text = `THIS FOR U PUPUNYA MAS`.split('')
 
 // Create container for better responsive layout
 title.style.display = 'flex'
